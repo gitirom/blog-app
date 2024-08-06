2d7e5dcdc9475ba3c1d5196332c226cefc8075cf
gitirom <wadiaromdane10@gmail.com>
2024-08-06 23:33:18 +0200

    complete the about page design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,42 @@ import styles from './about.module.css';
 
 const aboutPage = () => {
     return (
-        <div>
+        <div className={styles.container} >
+            <div className={styles.textContainer}>
+                <h2 className={styles.subTitle} >About Agency</h2>
+                <h1 className={styles.title} >
+                    We create digital ideas
+                    that are bigger, bolder,
+                    braver and better.
+                </h1>
+                <p className={styles.desc} >
+                    We create digital ideas that are bigger, bolder, braver and better. We
+                    believe in good ideas flexibility and precission We're world's Our Special
+                    Team best consulting & finance solution provider. Wide range of web
+                    and software development services.
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10 k+</h1>
+                        <p>Year of exprience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 k+</h1>
+                        <p>Year of exprience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 k+</h1>
+                        <p>Year of exprience</p>
+                    </div>
+                </div>
+            </div>
             <div className={styles.imgContiner}>
-                <Image src='https://images.pexels.com/photos/4240505/pexels-photo-4240505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="" fill   />
+                <Image 
+                    src='/about.png' 
+                    alt="About Image" 
+                    fill
+                    className={styles.img}
+                />
             </div>
         </div>
     )
